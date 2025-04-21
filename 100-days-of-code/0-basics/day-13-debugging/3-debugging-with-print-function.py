3ec7d22a0b7dfff6a9d68b692868764651ca85b9
datahogger <thebinaryhack@gmail.com>
2025-04-21 19:48:37 -0400

    day-13

---
+++ 100-days-of-code/0-basics/day-13-debugging/3-debugging-with-print-function.py
@@ -0,0 +1,9 @@
+words_per_page = 0
+pages = int(input("Enter the number of pages: "))
+words_per_page = int(input("Enter the number of words per page: "))
+
+total_words = pages * words_per_page
+
+print(f"pages: {pages}")
+print(f"words_per_page: {words_per_page}")
+print(f"total_words: {total_words}")
