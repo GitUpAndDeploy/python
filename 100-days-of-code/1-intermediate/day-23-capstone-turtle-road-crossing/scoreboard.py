df9ddd836660123d5d0039b0282e1ab322998b52
gitupanddeploy <thebinaryhack@gmail.com>
2025-04-26 21:08:52 -0400

    day-23

---
+++ 100-days-of-code/1-intermediate/day-23-capstone-turtle-road-crossing/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+  def __init__(self):
+    super().__init__()
+    self.color("black")
+    self.penup()
+    self.hideturtle()
+    self.level = 1
+    self.update_scoreboard()
+  
+  def update_scoreboard(self):
+    self.clear()
+    self.goto(-200,260)
+    self.write(f"Level: {str(self.level)}", align=ALIGNMENT, font=FONT)
+  
+  def next_level(self):
+    self.level += 1
+    self.update_scoreboard()
+  
+  def game_over(self):
+    self.goto(0,0)
+    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
